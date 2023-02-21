bb6366c81199f213042f31b84e245a6b7ac84b31
Ahsan Sium <ahsansium28@gmail.com>
2023-02-21 22:31:26 +0600

    Home Page About Page & Contact Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import "./index.scss";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomeTwo from "./components/HomeTwo";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NoMAtch from "./pages/404";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          MBSTU E LEARNING
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Project Initialized
-        </a>
-      </header>
+      <BrowserRouter basename={"/"} >
+        <Switch>
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/`}
+            component={HomeTwo}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/home-two`}
+            component={HomeTwo}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/about-us`}
+            component={About}
+          />
+
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/contact-us`}
+            component={Contact}
+          />
+
+
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/*`}
+            component={NoMAtch}
+          />
+
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/store.js
@@ -0,0 +1,23 @@
+import { applyMiddleware, compose } from "redux";
+import { legacy_createStore as createStore } from 'redux'
+import thunk from "redux-thunk";
+import rootReducer from "./reducers"; //index.js file
+
+//initital state for store
+const initialState = {};
+
+//any other middleware add here
+const middleware = [thunk];
+
+//1st arg: reducer, 2nd arg: state, 3rd: enhancer
+const store = createStore(
+  rootReducer,
+  initialState,
+
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
